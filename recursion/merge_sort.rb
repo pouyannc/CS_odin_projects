6021bb98656e98fc3e96f96d14e979f1c751665c
Pouyan <pouyannc@gmail.com>
2022-11-06 18:05:14 -0500

    Create merge sort method

---
+++ recursion/merge_sort.rb
@@ -0,0 +1,22 @@
+def merge_sort(arr1, arr_merged = [])
+  return arr1 if arr1.length == 1
+  
+  arr2 = arr1.pop(arr1.length/2)
+  arr1 = merge_sort(arr1)
+  arr2 = merge_sort(arr2)
+
+  loop do
+    arr1[0] < arr2[0] ? arr_merged.push(arr1.shift) : arr_merged.push(arr2.shift)
+    if arr1.empty? 
+      arr_merged.push(arr2)
+      break
+    elsif arr2.empty?
+      arr_merged.push(arr1)
+      break
+    end
+  end
+
+  arr_merged.flatten
+end
+
+p merge_sort([4,8,6,2,1,7,5,3])
